9b397a4c781c604dfcf2f4a5a7f662ff73490d8f
Quallenauge <Hamsi2k@freenet.de>
2021-01-18 21:18:14 +0100

    power: Re-Enable power_hint() call for Mode::LAUNCH.
    This hint is handled by most supported targets.
    
    Change-Id: I9e02dd50e748bde078c2c7588f4f60dcae9027dc

--- Power.cpp
+++ Power.cpp
@@ -79,7 +79,6 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
         case Mode::DOUBLE_TAP_TO_WAKE:
 #endif
         case Mode::LOW_POWER:
-        case Mode::LAUNCH:
         case Mode::DEVICE_IDLE:
         case Mode::DISPLAY_INACTIVE:
         case Mode::AUDIO_STREAMING_LOW_LATENCY:
@@ -92,6 +91,9 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
             break;
         case Mode::EXPENSIVE_RENDERING:
             set_expensive_rendering(enabled);
+            break;
+        case Mode::LAUNCH:
+            power_hint(POWER_HINT_LAUNCH, enabled ? &enabled : NULL);
             break;
         case Mode::INTERACTIVE:
             setInteractive(enabled);
@@ -120,6 +122,7 @@ ndk::ScopedAStatus Power::isModeSupported(Mode type, bool* _aidl_return) {
         case Mode::DOUBLE_TAP_TO_WAKE:
 #endif
         case Mode::EXPENSIVE_RENDERING:
+        case Mode::LAUNCH:
         case Mode::INTERACTIVE:
         case Mode::SUSTAINED_PERFORMANCE:
         case Mode::FIXED_PERFORMANCE:
