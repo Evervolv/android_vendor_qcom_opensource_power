35aad6ff85a6354083856aac9b2abf74a1bcd3fe
Jeongik Cha <jeongik@google.com>
2021-02-15 17:48:31 +0900

    Specify version for aidl_interface explicitly
    Bug: 150578172
    Test: m
    Change-Id: I9714cd4416fe0ce23e57cd7db162bfb45b0f84d5

--- Android.mk
+++ Android.mk
@@ -12,8 +12,8 @@ LOCAL_SHARED_LIBRARIES := \
     libdl \
     libbase \
     libutils \
-    android.hardware.power-ndk_platform \
-    vendor.evervolv.power-ndk_platform \
+    android.hardware.power-V1-ndk_platform \
+    vendor.evervolv.power-V1-ndk_platform \
     libbinder_ndk
 
 LOCAL_HEADER_LIBRARIES := \
